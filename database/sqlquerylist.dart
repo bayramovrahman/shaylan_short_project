2fc2a65a05a9b8a1f77c803b9eb5463dfb9e04a3
SHAYLAN\rahman <bayramovrahman@gmail.com>
2025-11-07 18:04:43 +0500

    Added creditReportLines sql

--- database/sqlquerylist.dart
+++ database/sqlquerylist.dart
@@ -1,3 +1,12 @@
+String createCreditReportLine =
+    '''CREATE TABLE creditReportLines ("route" TEXT, "district" TEXT, "agent" TEXT, "docNum" INTEGER, 
+    "deliveriDate" TEXT, "paymentType" TEXT, "client" TEXT, "cardCode" TEXT, "clientBalance" REAL, 
+    "ostalos" REAL, "expired" INTEGER, "creditSum" REAL, "consignmentTotal" REAL, 
+    "sumOplaty" REAL, "sumVozvrat" REAL, "orderSum" REAL , "balance" REAL, "credit7" REAL, 
+    "credit14" REAL, "credit30" REAL, "transfer" REAL, "transId" INTEGER, "u_Faktura_Group" TEXT, 
+    "docEntryInv" INTEGER, "docNumInv" INTEGER, "docEntryRin" INTEGER, "docNumRin" INTEGER);''';
+
+
 String createTerminal =
     '''CREATE TABLE terminals ("itemCode" TEXT, "itemName" TEXT, 
     "assetSerNo" TEXT, "assetGroup" TEXT, "account" TEXT);''';
